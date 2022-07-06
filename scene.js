d8587055cd2b5582b742c3cfd784f1486bc0889e
Eli <landaed96@gmail.com>
2022-07-06 16:28:44 -0700

    changed website to be a new concept

--- scene.js
+++ scene.js
@@ -1,11 +1,13 @@
+
+
 var canvas = document.getElementById("renderCanvas");
 
 var startRenderLoop = function (engine, canvas) {
-  engine.runRenderLoop(function () {
-      if (sceneToRender && sceneToRender.activeCamera) {
-          sceneToRender.render();
-      }
-  });
+    engine.runRenderLoop(function () {
+        if (sceneToRender && sceneToRender.activeCamera) {
+            sceneToRender.render();
+        }
+    });
 }
 
 var engine = null;
@@ -13,149 +15,134 @@ var scene = null;
 var sceneToRender = null;
 var createDefaultEngine = function() { return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false}); };
 
+
+BABYLON.UniversalCamera.prototype.spinTo = function (whichprop, targetval, speed) {
+    const targetPropertyPath = whichprop.split(".");
+    let value = this[targetPropertyPath[0]];
+    for (var index = 1; index < targetPropertyPath.length; index++) {
+        value = value[targetPropertyPath[index]];
+    }
+
+    var ease = new BABYLON.CubicEase();
+    ease.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+	BABYLON.Animation.CreateAndStartAnimation('at4', this, whichprop, speed, 120, value, targetval, 0, ease);
+}
 var createScene = function () {
 // This creates a basic Babylon Scene object (non-mesh)
 var scene = new BABYLON.Scene(engine);
 
-var camera = new BABYLON.ArcRotateCamera("camera", BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(65), 10, BABYLON.Vector3.Zero(), scene);
+// This creates and positions a free camera (non-mesh)
+var camera = new BABYLON.UniversalCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+
+
 
 // This attaches the camera to the canvas
 camera.attachControl(canvas, true);
 
 // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+var light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(1, 10, -10), scene);
 
 // Default intensity is 1. Let's dim the light a small amount
-light.intensity = 0.7;
+light.intensity = 0.5;
 
-// Our built-in 'ocean' shape.
-var ocean = BABYLON.MeshBuilder.CreateGround("ocean", {width: 600, height: 600, subdivisions: 256, updatable: true}, scene);
-var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 600, height: 600, subdivisions: 256, updatable: true}, scene);
-ground.position.y =10;
-var oceanShaderMat = new BABYLON.ShaderMaterial("shader", scene, "./oceanShader", {
-  attributes: ["position", "normal", "uv", "colour1", "colour2"],
-  uniforms: [
-      "world", "worldView", "worldViewProjection", "view",
-      "projection", "albedoMatrix", "cameraDirection", "camPos"
-  ],
-  defines: ["NORMAL"]
-});
+// Our built-in 'sphere' shape.
+const wallF = BABYLON.MeshBuilder.CreatePlane("wallF", {height:100, width: 500});
 
-var groundShaderMat = new BABYLON.ShaderMaterial("shader", scene, "./groundShader", {
-  attributes: ["position", "normal", "uv", "colour1", "colour2"],
-  uniforms: [
-      "world", "worldView", "worldViewProjection", "view",
-      "projection", "albedoMatrix", "cameraDirection", "camPos"
-  ],
-  defines: ["NORMAL"]
-});
-
-let oceanTexture = new BABYLON.Texture(Assets.textures.checkerboard_basecolor_png.rootUrl, scene);
-oceanShaderMat.setTexture("textureSampler", oceanTexture);
-oceanShaderMat.alpha = false;
-let groundTexture = new BABYLON.Texture(Assets.textures.checkerboard_basecolor_png.rootUrl, scene);
-groundShaderMat.setTexture("textureSampler", groundTexture);
-
-oceanShaderMat.setFloat("waveScale", .1);
-oceanShaderMat.setFloat("waveSpeed", 1);
-
-groundShaderMat.setFloat("groundScale", .1);
-groundShaderMat.setFloat("groundOffset", 10);
-groundShaderMat.setFloat("groundHeight", 30);
-//
-ocean.material = oceanShaderMat;
-ground.material = groundShaderMat;
-BABYLON.SceneLoader.ImportMesh("", Assets.meshes.Yeti.rootUrl, Assets.meshes.Yeti.filename, scene, function(newMeshes){
-  newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
-});
-var time = 0;
-
-scene.onBeforeRenderObservable.add(() => {
-  time+=.001;
-
-  oceanShaderMat.setFloat("iTime", time*50);
-  oceanShaderMat.setFloat("time", time*50);
-  //groundShaderMat.setFloat("groundOffset", time*50);
-  oceanShaderMat.setColor3("camPos", new BABYLON.Color3(camera.globalPosition.x,camera.globalPosition.y,camera.globalPosition.z));
-  });
-
-  // GUI
-
-  var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-
-  var panel = new BABYLON.GUI.StackPanel();
-  panel.width = "220px";
-  panel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
-  panel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
-  advancedTexture.addControl(panel);
-
-  var header = new BABYLON.GUI.TextBlock();
-  header.text = "Wave Scale: 0.1";
-  header.height = "30px";
-  header.color = "white";
-  panel.addControl(header);
-
-  var slider = new BABYLON.GUI.Slider();
-  slider.minimum = 0;
-  slider.maximum = 1.;
-  slider.value = 0;
-  slider.isVertical = true;
-  slider.height = "200px";
-  slider.width = "20px";
-  slider.onValueChangedObservable.add(function(value) {
-      header.text = "Wave Scale: " + value;
-      oceanShaderMat.setFloat("waveScale", value);
-  });
-  panel.addControl(slider);
+// Move the sphere upward 1/2 its height
+wallF.position.y = 1;
+wallF.position.z = 10;
 
 
-  var speedPanel = new BABYLON.GUI.StackPanel();
-  speedPanel.width = "220px";
-  speedPanel.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
-  speedPanel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
-  advancedTexture.addControl(speedPanel);
+// Our built-in 'sphere' shape.
+const wallB = BABYLON.MeshBuilder.CreatePlane("wallB", {height:100, width: 500});
 
-  var speedHeader = new BABYLON.GUI.TextBlock();
-  speedHeader.text = "Wave Speed: 0.1";
-  speedHeader.height = "30px";
-  speedHeader.color = "white";
-  speedPanel.addControl(speedHeader);
+// Move the sphere upward 1/2 its height
+wallB.rotation.y=3.1415;
+wallB.position.y = 1;
+wallB.position.z = -30;
 
-  var speedSlider = new BABYLON.GUI.Slider();
-  speedSlider.minimum = 0;
-  speedSlider.maximum = 100.;
-  speedSlider.value = 0;
-  speedSlider.isVertical = true;
-  speedSlider.height = "200px";
-  speedSlider.width = "20px";
-  speedSlider.onValueChangedObservable.add(function(value) {
-      speedHeader.text = "Wave Speed: " + value;
-      oceanShaderMat.setFloat("waveSpeed", value);
-  });
-  speedPanel.addControl(speedSlider);
+// Our built-in 'sphere' shape.
+const wallR = BABYLON.MeshBuilder.CreatePlane("wallR", {height:100, width: 100});
+
+// Move the sphere upward 1/2 its height
+wallR.rotation.y=1;
+wallR.position.y = 1;
+wallR.position.x = 10;
+
+// Our built-in 'sphere' shape.
+const wallL = BABYLON.MeshBuilder.CreatePlane("wallL", {height:100, width: 100});
+
+// Move the sphere upward 1/2 its height
+wallL.rotation.y=-1;
+wallL.position.y = 1;
+wallL.position.x = -10;
+var panelArray = [];
+var panelCount = 10;
+const panel = BABYLON.MeshBuilder.CreatePlane("panel", {height: 2, width: 2});
+for (var i = 0; i < panelCount; i++) {
+    var radians = 2 * 3.1415 / panelCount * i;
+    var vertical = Math.sin(radians);
+    var horizontal = Math.cos(radians);
+    let panelClone = panel.clone("panelClone" + i);
+
+    panelArray.push(panelClone);
+
+    panelClone.position.y = 6;
+    panelClone.position.x = camera.position.x+vertical*6;
+    panelClone.position.z = camera.position.z+horizontal*6;
+    panelClone.lookAt(camera.position);
+    panelClone.rotation.y += BABYLON.Tools.ToRadians(180);
+  }
+
+// This targets the camera to scene origin
+camera.inputs.attached.mouse.detachControl();
+camera.inputs.attached.keyboard.detachControl();
+
+var curr = 0;
+camera.setTarget(panelArray[curr].position);
+var canScroll = true;
+
+window.addEventListener('mousemove', function(event){
+         if(event.clientX/window.innerWidth > .5 && canScroll){
+           time = 0;
+           curr++;
+           if (curr >= panelCount-1){
+             curr = 0;
+           }
+           console.log("curr++: " +(180/3.14159)*curr*3.14159/panelCount);
+           setTimeout(()=>camera.spinTo("rotation.y", -curr*2*3.14159/panelCount, 120), 10);
+           canScroll = false;
+         }
+         if(event.clientX/window.innerWidth < .5 && !canScroll){
+           canScroll = true;
+         }
+    })
+
+// Our built-in 'ground' shape.
+var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 500, height: 600}, scene);
 
 return scene;
 };
-          window.initFunction = async function() {
+        window.initFunction = async function() {
 
 
-              var asyncEngineCreation = async function() {
-                  try {
-                  return createDefaultEngine();
-                  } catch(e) {
-                  console.log("the available createEngine function failed. Creating the default engine instead");
-                  return createDefaultEngine();
-                  }
-              }
+            var asyncEngineCreation = async function() {
+                try {
+                return createDefaultEngine();
+                } catch(e) {
+                console.log("the available createEngine function failed. Creating the default engine instead");
+                return createDefaultEngine();
+                }
+            }
 
-              window.engine = await asyncEngineCreation();
-  if (!engine) throw 'engine should not be null.';
-  startRenderLoop(engine, canvas);
-  window.scene = createScene();};
-  initFunction().then(() => {sceneToRender = scene
-  });
+            window.engine = await asyncEngineCreation();
+if (!engine) throw 'engine should not be null.';
+startRenderLoop(engine, canvas);
+window.scene = createScene();};
+initFunction().then(() => {sceneToRender = scene
+});
 
-  // Resize
-  window.addEventListener("resize", function () {
-      engine.resize();
-  });
+// Resize
+window.addEventListener("resize", function () {
+    engine.resize();
+});
