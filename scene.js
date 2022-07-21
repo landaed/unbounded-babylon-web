4a6f79b942976ec38a0bdda914c7e2b02e6eaeb4
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2022-07-21 07:28:03 -0700

    added panels and lighting to the website as well as simchop logo

--- scene.js
+++ scene.js
@@ -1,3 +1,6 @@
+//Boiler plate setup
+
+//------------------------------------------------------
 var canvas = document.getElementById("renderCanvas");
 
 var startRenderLoop = function (engine, canvas) {
@@ -12,61 +15,193 @@ var engine = null;
 var scene = null;
 var sceneToRender = null;
 var createDefaultEngine = function() { return new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true,  disableWebGL2Support: false}); };
+//-------------------------------------------------------
 var createScene = function () {
-    var htmlBG = document.createElement("PRE");
-    htmlBG.style.zIndex = -1;
-    htmlBG.style.position = "absolute"
-    htmlBG.style.width = "100VW";
-    htmlBG.style.height = "100VH";
-    htmlBG.style.left = 0;
-    htmlBG.style.top = 0;
-    htmlBG.style.whiteSpace = "pre";
-    htmlBG.style.backgroundImage = "linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet)";
-    htmlBG.innerText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Ac turpis egestas integer eget aliquet nibh. Egestas dui id ornare arcu odio ut sem nulla pharetra. Ipsum dolor sit amet consectetur adipiscing elit duis. Enim lobortis scelerisque fermentum dui. Nulla facilisi cras fermentum odio eu feugiat pretium nibh. Lectus nulla at volutpat diam ut venenatis. Cursus sit amet dictum sit amet justo donec enim. Sed risus ultricies tristique nulla aliquet. Blandit cursus risus at ultrices mi. Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Lobortis elementum nibh tellus molestie nunc non. Magnis dis parturient montes nascetur ridiculus. Semper auctor neque vitae tempus quam.`;
-    document.getElementById("canvasZone").appendChild(htmlBG);
+  var scene = new BABYLON.Scene(engine);
+
+  //Adding a light
+  var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 3, 0), scene);
+  light.intensity = 50;
+  var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3( -15, 3,0), scene);
+  light2.intensity = 50;
+  var light3 = new BABYLON.PointLight("light3", new BABYLON.Vector3(15, 3, -0), scene);
+  light3.intensity = 50;
+
+  //Adding a light
+  var light4 = new BABYLON.PointLight("light4", new BABYLON.Vector3(0, 3, -10), scene);
+  light4.intensity = 50;
+  var light5 = new BABYLON.PointLight("light5", new BABYLON.Vector3( -15, 3,-10), scene);
+  light5.intensity = 50;
+  var light6 = new BABYLON.PointLight("light6", new BABYLON.Vector3(15, 3, -10), scene);
+  light6.intensity = 50;
+
+  //Adding a light
+  var light7 = new BABYLON.PointLight("light7", new BABYLON.Vector3(0, 3, -20), scene);
+  light7.intensity = 50;
+  var light8 = new BABYLON.PointLight("light8", new BABYLON.Vector3( -15, 3,-20), scene);
+  light8.intensity = 50;
+  var light9 = new BABYLON.PointLight("light9", new BABYLON.Vector3(15, 3, -20), scene);
+  light9.intensity = 50;
+
+  //Adding an Arc Rotate Camera
+  var camera = new BABYLON.UniversalCamera("camera1", new BABYLON.Vector3(1, 1, 4), scene);
+  camera.rotation.y = 3.1415;
+  //load office
+  var office = BABYLON.SceneLoader.ImportMesh("", "./models/", "laboratory.glb", scene, function (newMeshes) {
+    newMeshes[0].position.y-=10;
+    newMeshes[0].position.z=-15;
+  });
+  office.renderingGroupId = 1;
+  //add floor
+  //const ground = BABYLON.MeshBuilder.CreateBox("ground", {width: 30, height: 1, depth: 30});
+  //ground.position.y = -3;
+  // add researchPanels
+   const researchPanel = BABYLON.MeshBuilder.CreatePlane("researchPanel", {height:.3, width: .3});
+   const researchPanelMask = BABYLON.MeshBuilder.CreatePlane("panelMask", {height:.3, width: .3});
+
+   researchPanel.position.x = 2;
+   researchPanel.position.z = 2;
+   researchPanel.position.y = 1;
+   var researchRot = 0;
+   researchPanel.rotation.y += BABYLON.Tools.ToRadians(180);
+   researchPanel.isPickable = false;
+   researchPanel.renderingGroupId = 2;
+   var maskMat = new BABYLON.StandardMaterial("maskMat", scene);
+   researchPanelMask.material = maskMat;
+   maskMat.alpha=0.5;
+   var researchPanelMat = new BABYLON.ShaderMaterial("shader", scene, "./bunnyShader", {
+     attributes: ["position", "normal", "uv", "colour1", "colour2"],
+     uniforms: [
+         "world", "worldView", "worldViewProjection", "view",
+         "projection", "albedoMatrix", "cameraDirection", "camPos"
+     ],
+     defines: ["NORMAL"]
+   });
+   researchPanel.material = researchPanelMat;
+//--------------------------------------------------------
+// camera logic
+  var angleX;
+  var angleY;
+
+  window.addEventListener('mousemove', function(event){
+     mousemovef();
+     angleX = 3.1415+(event.clientX/window.innerWidth-.5)*2;
+     angleY = (event.clientY/window.innerHeight-.5)*2.;
+   });
+
+   // desiredFps that is greater than screen's refresh rate will have no effect
+  // eg 120 fps still looks like 60fps on a 60hz monitor
+  var desiredFps = 60;
+  var interval = 1000/desiredFps;
+  var lastTime = performance.now();
+  var incrementX =0;
+  var incrementY = 0;
+  function gameLogic() {
+      //console.log("angleX " + angleX + "cam.y" + camera.rotation.y);
+      // game logic or physics, etc
+      if (camera.rotation.y < angleX -.1 && camera.rotation.y < 3.5) {
+          incrementX = 0.001
+      }else if (camera.rotation.y > angleX +.1&& camera.rotation.y > 2.9) {
+          incrementX = -0.001
+      }
+      else{
+        incrementX=0;
+      }
+      if (camera.rotation.x < angleY -.1 && camera.rotation.x < .1) {
+          incrementY = 0.001
+      }else if (camera.rotation.x > angleY +.1&& camera.rotation.x > -.1) {
+          incrementY = -0.001
+      }
+      else{
+        incrementY=0;
+      }
+      camera.rotation.y += incrementX;
+      camera.rotation.x += incrementY;
+      researchRot = new BABYLON.Vector3(camera.rotation.x,camera.rotation.y,camera.rotation.z);
+  }
+  scene.onBeforeRenderObservable.add(() => {
+  //  time+=.001;
+  //  console.log(camera.getDirection(BABYLON.Vector3.Forward()));
+    var forward = camera.getDirection(BABYLON.Vector3.Forward());
+    var left = camera.getDirection(BABYLON.Vector3.Left());
+    var up = camera.getDirection(BABYLON.Vector3.Up());
+    var dist = 2;
+    var leftOff = .3;
+    var upOff = -0.7;
+    //forward=2;
+  //  researchPanel.position = camera.position.x;
+    researchPanel.position.x = camera.position.x + dist*forward.x + leftOff*left.x + upOff*up.x;
+    researchPanel.position.y = camera.position.y + dist*forward.y + leftOff*left.y + upOff*up.y;
+    researchPanel.position.z = camera.position.z + dist*forward.z + leftOff*left.z + upOff*up.z;
+    dist = 1.9;
+    upOff = -0.66;
+    leftOff = .29;
+    researchPanelMask.position.x = camera.position.x + dist*forward.x + leftOff*left.x + upOff*up.x;
+    researchPanelMask.position.y = camera.position.y + dist*forward.y + leftOff*left.y + upOff*up.y;
+    researchPanelMask.position.z = camera.position.z + dist*forward.z + leftOff*left.z + upOff*up.z;
 
 
+  researchPanelMask.rotation=camera.rotation;
 
-    var scene = new BABYLON.Scene(engine);
+    if(pickResult){
 
+        if(pickResult.pickedMesh){
+        //  console.log(pickResult.pickedMesh.name);
+          var c = pickResult.getTextureCoordinates();
+          if(c && pickResult.pickedMesh.name == "panelMask"){
+            c.x -=.5;
+            c.y -=.5;
+            var rotBounds = .4;
+            var yOff = 0;
 
-    // Make background clear.
-    scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+            if (researchPanel.rotation.y - researchRot.y < 1 && c.x <0) {
+            //    researchPanel.rotation.y += Math.abs(c.x)*.1;
+            }
+            if (researchPanel.rotation.y - researchRot.y > -1 && c.x >0) {
+            //    researchPanel.rotation.y -= Math.abs(c.x)*.1;
+            }
+            if (researchPanel.rotation.x - researchRot.x < 1 && c.y > 0) {
+            //    researchPanel.rotation.x += Math.abs(c.y)*.1;
+            }
+            if (researchPanel.rotation.x - researchRot.x > -1 && c.y < 0) {
+            //    researchPanel.rotation.x -= Math.abs(c.y)*.1;
+            }
+          }
+        }
+      }
+      else{
+        researchPanel.rotation=camera.rotation;
+      }
 
+  });
 
-    var camera = new BABYLON.ArcRotateCamera("camera1", 2.9, 1.0, 12.0, BABYLON.Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-        sphere.position.y = 1;
-        sphere.position.x = 0;
-    var m = new BABYLON.StandardMaterial('smat', scene);
-    sphere.material = m;
+  // see this discussion: https://gist.github.com/addyosmani/5434533
+  // this will sync your game logic updates with the render updates
+  function loop() {
+      window.requestAnimationFrame(loop)
 
-    // TRY TO USE THIS SHAPE TO CUT A HOLE TO THE UNDERLYING HTML
-    m.alpha = 0.0;
-    m.alphaMode = BABYLON.Engine.ALPHA_DISABLE;
+      let currentTime = performance.now();
+      const deltaTime = currentTime - lastTime;
 
+      if (deltaTime > interval) {
+          lastTime = currentTime - (deltaTime % interval);
+          gameLogic();
+      }
+  }
+  loop();
+//----------------------------------------------------------
+var pickResult;
+function mousemovef(){
+  pickResult = scene.pick(scene.pointerX, scene.pointerY);
 
-  // Demonstrate the hole is working correctly with Z order of other objects.
-  var s2 = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-  s2.position.y = 1;
-  s2.position.x = -2;
-  s2.renderOverlay = true;
-  s2.overlayColor.copyFromFloats(1,0,0)
+  if (pickResult.hit) {
+    const conToB = BABYLON.Matrix.Identity();
+    var mPos = pickResult.pickedPoint;
 
-    var s3 = s2.createInstance();
-        s3.position.x = 2;
-
-
-
-    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-
-    return scene;
-
-};
+  }
+}
+  return scene;
+}
         window.initFunction = async function() {
 
 
