1400ab6b744c8af9fc438cc18ee6de49670d0695
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2023-04-06 13:37:02 -0700

    big update

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
-
-// Set the content-type for JavaScript files to 'text/javascript'
-express.static.mime.types['js'] = 'text/javascript';
+var mime    = require("mime");
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname+ '/public')));
+app.use(express.static(path.join(__dirname + '/public'), {
+  setHeaders: function (res, path) {
+    if (mime.getType(path) === 'text/javascript') {
+      res.setHeader('Content-Type', 'text/javascript');
+    }
+  }
+}));
 
 // Serve the index.html file
 app.get('/',function(req,res){
