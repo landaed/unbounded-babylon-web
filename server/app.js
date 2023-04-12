2c49d057dc3429eb5383a7801d9f77df7ec9674f
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2023-04-12 12:54:52 -0700

    added a server directory for local testing

---
+++ server/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+var mime    = require("mime");
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname + '/../'), {
+  setHeaders: function (res, path) {
+    if (mime.getType(path) === 'text/javascript') {
+      res.setHeader('Content-Type', 'text/javascript');
+    }
+  }
+}));
+
+// Serve the index.html file
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/../index.html'));
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
