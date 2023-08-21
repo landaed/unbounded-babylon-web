307cf32e636e7ac4159f0c581e9bddbb4185a7cc
PapaHunk <69608311+PapaHunk@users.noreply.github.com>
2023-08-21 02:16:32 -0700

    improved language and added graphics cirriculum.

---
+++ assets/js/expandable.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.card').forEach(section => {
+    section.addEventListener('click', function() {
+      if (this.classList.contains('expanded')) {
+        this.classList.remove('expanded');
+      } else {
+        // Collapse all cards
+        document.querySelectorAll('.card.expanded').forEach(sec => {
+          sec.classList.remove('expanded');
+        });
+  
+        // Expand the clicked card
+        this.classList.add('expanded');
+      }
+    });
+  });
+  
